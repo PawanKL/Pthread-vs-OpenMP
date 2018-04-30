f9eabcdcc0d8045c482aa3608bdb5e484e88a699
PawanKL <pawankumarluhana123@gmail.com>
2018-04-30 20:31:16 +0500

    main function added

--- merge_pthread.c
+++ merge_pthread.c
@@ -6,3 +6,31 @@
 void *mergesort_pthread(void *);
 void merge(int,int);
 int arr[MAX];
+
+struct ArrayIndex
+{
+ int low;
+ int high;
+ 
+};
+int main()
+{
+
+ struct ArrayIndex ai;
+ ai.low = 0;
+ ai.high =  MAX - 1;
+ pthread_t t;
+
+ // Generating Random Values...
+ 
+ printf("Unsorted Data\n");
+ srand(time(NULL));
+ int i;
+ for(i=0;i<MAX;i++)
+ {
+   arr[i] = rand() % MAX; 
+   printf("%d ",arr[i]); 
+
+ }
+}
+
