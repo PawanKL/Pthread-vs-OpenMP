8414f2f4c7bae6cae54c9c868606523618285669
PawanKL <pawankumarluhana123@gmail.com>
2018-04-30 21:03:36 +0500

    main function added

--- quickl_pthread.c
+++ quickl_pthread.c
@@ -10,3 +10,24 @@ struct ArrayIndex{
 int low;
 int high;
 };
+int main()
+{
+   int i,retval;
+   pthread_t thread;
+   struct ArrayIndex ai;
+   ai.low = 0;
+   ai.high = MAX - 1;
+   clock_t t1,t2;
+
+   
+   //Generating Random values..
+
+   srand(time(NULL));
+   printf("Unsorted Data\n");z
+   for(i=0;i<MAX;i++)
+   {
+	arr[i]=rand() % MAX;
+	printf("%d ",arr[i]);
+   }
+   printf("\n");
+}
