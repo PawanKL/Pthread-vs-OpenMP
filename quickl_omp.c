20d5ece055fb422fa1aee626f451bd0e13b0efaf
PawanKL <pawankumarluhana123@gmail.com>
2018-04-30 21:24:59 +0500

    partition function added

--- quickl_omp.c
+++ quickl_omp.c
@@ -3,3 +3,27 @@
 #include<time.h>
 #include<omp.h> 
 #define MAX 1000
+int partition (int arr[], int low, int high)
+{
+    int i,j,pivot = arr[high],temp;    // pivot
+    i = low - 1;  // Index of smaller element
+ 
+    for(j = low; j <= high - 1; j++)
+    {
+        // If current element is smaller than or
+        // equal to pivot
+        if (arr[j] <= pivot)
+        {
+            i++;    // increment index of smaller element
+            //swap(&arr[i], &arr[j]);
+	    temp = arr[i];
+	    arr[i] = arr[j];
+	    arr[j] = temp;
+        }
+    }
+    //swap(&arr[i + 1], &arr[high]);
+    temp = arr[i + 1];
+    arr[i + 1] = arr[high];
+    arr[high] = temp;
+    return (i + 1);
+}
