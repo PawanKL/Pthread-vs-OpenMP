4bbe5fec966f90fb10ce7c41d2e14f84ab4cf8c4
PawanKL <pawankumarluhana123@gmail.com>
2018-04-30 21:21:06 +0500

    partition function added

--- quickf_omp.c
+++ quickf_omp.c
@@ -3,3 +3,23 @@
 #include<time.h>
 #include<omp.h> 
 #define MAX 1000
+int partition (int arr[], int low, int high)
+{
+    int i,j,pivot = arr[low],temp; 
+    i = high + 1; 
+ 
+    for (j = high; j > 0; j--)
+    {
+        if (arr[j] > pivot)
+        {
+            i--;    
+	    temp = arr[i];
+	    arr[i] = arr[j];
+	    arr[j] = temp;
+        }
+    }
+    temp = arr[low];
+    arr[low] = arr[i-1];
+    arr[i-1] = temp;
+    return (i-1);
+}
