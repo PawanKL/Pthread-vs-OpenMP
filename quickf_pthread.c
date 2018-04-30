a72261873a4274a8df51cb2a4a9d5e02bb9acb83
PawanKL <pawankumarluhana123@gmail.com>
2018-04-30 20:47:20 +0500

    Headers and function prototypes

---
+++ quickf_pthread.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<pthread.h>
+#define MAX 100
+void *QuickSort_pthread(void *);
+int Partition(int,int);
+int arr[MAX];
+struct ArrayIndex{
+int low;
+int high;
+};
+
